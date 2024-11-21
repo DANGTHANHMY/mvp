21243d4ce6a3d3e2f4cdeb9fbb1aded12c4d05f7
DANGTHANHMY <dangthanhmyst@gmail.com>
2024-11-21 23:51:22 +0700

    sdfghj

--- src/data_utils.py
+++ src/data_utils.py
@@ -38,8 +38,6 @@ def get_orders(task, data, args, sents, labels):
     #                                         args.task)
 
     if args.single_view_type == 'rank':
-        print(optim_orders_all)
-        print(optim_orders_all[task][data])
         orders = optim_orders_all[task][data]
     elif args.single_view_type == 'rand':
         orders = [random.Random(args.seed).choice(
